93a22a7f873f748106d6ebe29da2cce67730287c
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-02-01 00:34:59 +0530

    Moved to a separate folder

--- src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.rest.webservices.restfulwebservices;
+package com.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
