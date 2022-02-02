a8dcb5a8b39364d2fab33da1efc404dedf23a581
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-02-03 00:39:46 +0530

    validation added

--- src/main/java/com/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,8 +2,10 @@ package com.rest.webservices.restfulwebservices.exception;
 
 import java.util.Date;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,10 +28,20 @@ ResponseEntityExceptionHandler {
 	}
 	
 	@ExceptionHandler(UserNotFoundException.class)
-	public final ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
+	public final ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex,
+			WebRequest request) {
 		ExceptionResponse exceptionResponse =new 
 				ExceptionResponse(new Date(),ex.getMessage(),request.getDescription(false));
 		
 		return new ResponseEntity(exceptionResponse,HttpStatus.NOT_FOUND);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, 
+			HttpStatus status, WebRequest request) {
+		ExceptionResponse exceptionResponse =new 
+				ExceptionResponse(new Date(),"validation failed",ex.getBindingResult().toString());
+		return new ResponseEntity(exceptionResponse,HttpStatus.BAD_REQUEST);
 	}
 }
--- src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
@@ -3,6 +3,8 @@ package com.rest.webservices.restfulwebservices.user;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -40,7 +42,7 @@ public class UserResource {
 	}
 	
 	@PostMapping("/users")
-	public ResponseEntity<Object> createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
 		User savedUser=service.saveUser(user);
 		URI location= ServletUriComponentsBuilder
 		.fromCurrentRequest()
