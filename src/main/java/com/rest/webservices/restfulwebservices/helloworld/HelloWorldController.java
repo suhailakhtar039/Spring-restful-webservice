667a681c30f60f6a8b6b493a0f935f6ec52a9741
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-02-08 22:30:02 +0530

    Added more functionality

--- src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,14 +1,10 @@
 package com.rest.webservices.restfulwebservices.helloworld;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContext;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- src/main/java/com/rest/webservices/restfulwebservices/user/UserNotFoundException.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/UserNotFoundException.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+
 	public UserNotFoundException(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
