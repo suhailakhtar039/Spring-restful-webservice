7b65fe74e203ca85d842c96583a91bc5328ca0bb
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-02-08 23:18:42 +0530

    Dynamic filtering added

--- src/main/java/com/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ src/main/java/com/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,11 +1,11 @@
 package com.rest.webservices.restfulwebservices.filtering;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonFilter;
 
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
 	private String field1;
 	private String field2;
-	@JsonIgnore
 	private String field3;
 	public SomeBean(String field1, String field2, String field3) {
 		super();
